87774711035f0bc55c424ad514fe62634d1ad859
Konrad Szmidt <kontakt@currant.pl>
2023-01-23 14:14:31 +0100

    model id

--- lib/src/models/calendar_event_model.dart
+++ lib/src/models/calendar_event_model.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 
 class CalendarEventModel {
   CalendarEventModel({
+    required this.id,
     required this.name,
     required this.begin,
     required this.end,
     this.eventColor = Colors.green,
   });
 
+  String id;
   String name;
   DateTime begin;
   DateTime end;
